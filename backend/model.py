6a7a07543b326455233813ac3c099abcc69b0348
Cyrus <u6858500@anu.edu.au>
2020-04-21 12:22:06 +1000

    Added a test for the randomly generated volunteers

--- backend/model.py
+++ backend/model.py
@@ -5,48 +5,6 @@ import sys
 from enum import Enum
 
 
-# returns a list populated with the the hours in a week to be scheduled
-def shiftpopulator():
-    list = []
-    # weeknumber can be added if need be by adding an extra forloop and the code could be very similar to the hours
-    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-    hours = range(24)
-    for i in days:
-        for j in hours:
-            # concatenating the day string with the hour to generate the label for an hour that is to be scheduled
-            list.append(i + str(j))
-    return list
-
-#establishing the two types of Firefighters
-class FireFighter(Enum):
-    basic = "Basic"
-    advanced = "Advanced"
-
-
-# returns a list of volunteers named Volunteer0 to VolunteerN
-# name generator for testing
-def volunteerList(volunteerNo):
-    list = []
-    for i in range(volunteerNo):
-        list.append("Volunteer" + str(i))
-    return list
-
-
-# each volunteer has an Name,Experience level, preferred Hours, Availability
-# is availability different
-class Volunteer:
-    def __init__(self, name, Explvl, prefHours):
-        self.name = name
-        self.Explvl = Explvl
-        self.prefHours = prefHours
-        AvailDict = {}
-        for i in shiftpopulator():
-            # generates 1 every 1/12 shift
-            AvailDict[i] = (int)(random.randrange(0, 12, 2) / 10)
-        self.Availability = AvailDict
-
-
-
 #Randomly generating a group of different Volunteers
 def volunteerGenerate(volunteerNo):
     list = []
@@ -59,7 +17,58 @@ def volunteerGenerate(volunteerNo):
         list.append(Volunteer("Volunteer" + str(i),exp,random.randrange(6,14,1)))
     return list
 
-Volunteers=volunteerGenerate(25)
+def VolunteerTest(number):
+    Volunteers=volunteerGenerate(number)
+    for i in Volunteers:
+            print("preferred Hours: "+str(i.prefHours))
+            print("Experience level: "+str(i.Explvl))
+            print(i.name+" Availability: ")
+            for j in shiftpopulator():
+                print(j+": "+str(i.Availability[j]))
+
+            print("\n")
+
+VolunteerTest(3)
+
+
+
+# returns a list populated with the the hours in a week to be scheduled
+def shiftpopulator():
+    list = []
+    # weeknumber can be added if need be by adding an extra forloop and the code could be very similar to the hours
+    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    hours = range(24)
+    for i in days:
+        for j in hours:
+            # concatenating the day string with the hour to generate the label for an hour that is to be scheduled
+            list.append(i + str(j))
+    return list
+
+
+
+
+
+#establishing the two types of Firefighters
+class FireFighter(Enum):
+    basic = "Basic"
+    advanced = "Advanced"
+# each volunteer has an Name,Experience level, preferred Hours, Availability
+# is availability different
+class Volunteer:
+    def __init__(self, name, Explvl, prefHours):
+        self.name = name
+        self.Explvl = Explvl
+        self.prefHours = prefHours
+        AvailDict = {}
+        for i in shiftpopulator():
+          AvailDict[i] = False
+          #generates a 0 or 1 and that gets converted to true or false using an if function
+          if (int)(random.randrange(0, 12, 2) / 10)==1:
+              AvailDict[i] =True
+        self.Availability = AvailDict
+
+
+
 
 # Model
 m = gp.Model("assignment")
