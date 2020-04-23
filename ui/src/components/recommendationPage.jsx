eae3f689809ef8827536264f447762a2fb577783
caleb <u6373233@anu.edu.au>
2020-04-23 15:06:29 +1000

    installed 'node-sass' package in the container

--- ui/src/components/recommendationPage.jsx
+++ ui/src/components/recommendationPage.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { Table } from "react-bootstrap";
+import AssetCrew from "./assetCrew";
 import { Button } from "react-bootstrap";
 
 class RecommendationPage extends Component {
   state = {
-    volunteers: [
+    vols: [
       { ID: 1, Pos: "Driver", Name: "Caleb Addison" },
       { ID: 2, Pos: "Crew Leader", Name: "Stavros Dimos" },
       { ID: 3, Pos: "Firefighter", Name: "Tom Willis" },
@@ -18,28 +18,9 @@ class RecommendationPage extends Component {
       //example format of a 'vehicle recommendation' component or something of the like
       <React.Fragment>
         <h4 className="mt-2">New Asset Request</h4>
-
-        <Table className="mt-4" striped bordered hover size="sm">
-          <thead>
-            <tr>
-              <th width="20%">Heavy Tanker 1</th>
-              <th colSpan="4">[time slot]</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.volunteers.map((a) => (
-              <tr key={a.assetID}>
-                <td>{a.Pos}</td>
-                <td>{a.Name}</td>
-                <td>[experience]</td>
-                <td>[phone number]</td>
-                <td>
-                  <Button className="btn-warning">Edit</Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+        <AssetCrew volunteers={this.state.vols} />
+        <AssetCrew volunteers={this.state.vols} />
+        <Button className="btn-med">Save</Button>
       </React.Fragment>
     );
   }
