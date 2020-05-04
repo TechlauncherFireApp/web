c684584087edec83f1ce000bc02481414a0a1f9e
Cyrus <u6858500@anu.edu.au>
2020-05-04 12:21:14 +1000

    Changed the assettype files to reflect the Request format on the Docs

--- backend/AssetTypes.py
+++ backend/AssetTypes.py
@@ -21,10 +21,12 @@ HeavyTanker = Asset(3, "Heavy Tanker", 5, 4)
 
 # StartTime and Duration in timeblocks
 class Request():
-    def __init__(self, AssetType, StartTime, Duration):
+    def __init__(self, AssetType, StartTime, EndTime):
         self.AssetType = AssetType
         self.StartTime = StartTime
-        self.Duration = Duration
+        self.EndTime = EndTime
+
+
 
 
 def RequesttoRequirements(Requests):
@@ -35,7 +37,7 @@ def RequesttoRequirements(Requests):
     for l in Requests:
         # This ensures we fill up all the hours that run between the starttime and the duration with the information
         # about the advanced and basic requirements
-        for k in range(l.StartTime, l.StartTime + l.Duration + 1):
+        for k in range(l.StartTime, l.EndTime):
             # checks if
             if k not in TotalRequirementDict:
                 TotalRequirementDict[k] = l.AssetType.TotalReq
@@ -43,7 +45,7 @@ def RequesttoRequirements(Requests):
                 # print("gets here")
                 TotalRequirementDict[k] += l.AssetType.TotalReq
     for l in Requests:
-        for k in range(l.StartTime, l.StartTime + l.Duration + 1):
+        for k in range(l.StartTime, l.EndTime):
             # checks if
             if k not in AdvancedRequirementDict:
                 AdvancedRequirementDict[k] = l.AssetType.AdvancedReq
@@ -62,33 +64,33 @@ def RequesttoRequirements(Requests):
 
 # Request Scenarios
 # 1700 monday onwards for 6.5hours
-SingleRequest = [Request(LightUnit, 34, 13)]
+SingleRequest = [Request(LightUnit, 34, 47)]
 
 # 1700 monday onwards for 6.5hours but two asset types
-TwoAssetTypeRequest = [Request(LightUnit, 34, 13), Request(MediumTanker, 34, 13)]
+TwoAssetTypeRequest = [Request(LightUnit, 34, 47), Request(MediumTanker, 34, 47)]
 
 # Multiple Days
-MultipleDaysRequest = [Request(LightUnit, 34, 13), Request(MediumTanker, 34, 13)
-    , Request(MediumTanker, 82, 13), Request(HeavyTanker, 82, 13)]
+MultipleDaysRequest = [Request(LightUnit, 34, 48), Request(MediumTanker, 34, 48)
+    , Request(MediumTanker, 82, 96), Request(HeavyTanker, 82, 96)]
 
 # OnlyWeekends
-OnlyWeekends = [Request(MediumTanker, 18 + 5 * 48, 24), Request(LightUnit, 18 + 5 * 48, 24),
-                Request(MediumTanker, 18 + 6 * 48, 10), Request(LightUnit, 18 + 6 * 48, 24)]
+OnlyWeekends = [Request(MediumTanker, 18 + 5 * 48, 18 + 5 * 48+24), Request(LightUnit, 18 + 5 * 48, 18 + 5 * 48+24),
+                Request(MediumTanker, 18 + 6 * 48,18 + 6 * 48+ 10), Request(LightUnit, 18 + 6 * 48,18 + 6 * 48+ 24)]
 # All Weekdays
-EveryWeekday = [Request(LightUnit, 34, 13), Request(MediumTanker, 34, 13)
-    , Request(MediumTanker, 82, 13), Request(HeavyTanker, 82, 13),
-                Request(MediumTanker, 34 + 2 * 48, 13), Request(HeavyTanker, 34 + 2 * 48, 13),
-                Request(MediumTanker, 34 + 3 * 48, 13), Request(HeavyTanker, 34 + 3 * 48, 13),
-                Request(MediumTanker, 34 + 4 * 48, 13), Request(LightUnit, 34 + 4 * 48, 13)
+EveryWeekday = [Request(LightUnit, 34, 47), Request(MediumTanker, 34, 47)
+    , Request(MediumTanker, 82, 95), Request(HeavyTanker, 82, 95),
+                Request(MediumTanker, 34 + 2 * 48, 34 + 2 * 48+13), Request(HeavyTanker, 34 + 2 * 48, 34 + 2 * 48+13),
+                Request(MediumTanker, 34 + 3 * 48, 34 + 3 * 48 + 13), Request(HeavyTanker, 34 + 3 * 48, 34 + 3 * 48 + 13),
+                Request(MediumTanker, 34 + 4 * 48, 34 + 4 * 48 + 13), Request(LightUnit, 34 + 4 * 48, 34 + 4 * 48 + 13)
                 ]
 # All Days of hte week
 ##solves all but this one with 60 volunteers generated it solved with 90 though
 # This one is only weekends and EveryWeekday combined
-EveryDayOfTheWeek = OnlyWeekends + EveryWeekday
+EveryDayOfTheWeek = EveryWeekday+OnlyWeekends
 
 
-def Test():
+def Test(Request):
     # A List of Requests
+    for i in RequesttoRequirements(Request).keys():
+        print(str(i) + ": " + str(RequesttoRequirements(Request)[i]))
 
-    for i in range(25):
-        print(str(i) + ": " + str(RequesttoRequirements(MultipleDaysRequest)[i]))
