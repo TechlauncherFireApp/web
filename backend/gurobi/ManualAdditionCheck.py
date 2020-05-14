efd4b9539946c84b073debe39d2a548b354a8e06
Tom Willis <u6377372@anu.edu.au>
2020-05-14 21:33:45 +1000

    Modified ManualAdditionCheck to work with interface. Uses defined objects rather than dictionaries.

--- backend/gurobi/ManualAdditionCheck.py
+++ backend/gurobi/ManualAdditionCheck.py
@@ -1,4 +1,4 @@
-from backend.gurobi.Scheduler import *
+from gurobi.Scheduler import *
 
 def NameToAsset(Name):
     if(Name=="Light Unit"):
@@ -10,73 +10,73 @@ def NameToAsset(Name):
 
 
 
-def ManualAdditionCheck(RecommendationList):
-    for vehicle in RecommendationList:
-        #Loading the type of vehicle
-        Type=NameToAsset(vehicle["asset_class"])
-        TotalNum=0
-        AdvancedNum=0
-        DriverNum=0
-        CrewLeaderNum=0
+def ManualAdditionCheck(asset_request, assigned_volunteers):
+    # for vehicle in RecommendationList:
+    #Loading the type of vehicle
+    Type=asset_request.AssetType
+    TotalNum=0
+    AdvancedNum=0
+    DriverNum=0
+    CrewLeaderNum=0
+
+    print("starting")
+
+    for Vol in assigned_volunteers:
 
 
+        #adds one to the total
+        TotalNum+=1
+        if (Vol.Explvl == FireFighter.basic.value):
+            #if Volunteer assigned to a job they're not qualified for
+            if(Vol.role!="Crew Member"):
+                print(Vol.name+ " id "+str(Vol.id)+" not qualified for "+Vol.role)
+                return False,Vol.name+ " id "+str(Vol.id)+" not qualified for "+Vol.role
 
-        for Vol in vehicle["volunteers"]:
-            #adds one to the total
-            TotalNum+=1
-            if (Vol["volunteer_explvl"] == FireFighter.basic.value):
-                #if Volunteer assigned to a job they're not qualified for
-                if(Vol['role']!="Crew Member"):
-                    print(Vol["volunteer_name"]+ " id "+str(Vol["volunteer_id"])+" not qualified for "+Vol['role'])
-                    return False,Vol["volunteer_name"]+ " id "+str(Vol["volunteer_id"])+" not qualified for "+Vol['role']
+        if (Vol.Explvl == FireFighter.advanced.value):
+            AdvancedNum += 1
+            #if Volunteeer assigned to a job they're not qualified for
+            if(Vol.role!="Crew Member"):
+                print(Vol.name+ " id "+str(Vol.id)+" not qualified for "+Vol.role)
+                return False,Vol.name+ " id "+str(Vol.id)+" not qualified for "+Vol.role
 
-            if (Vol["volunteer_explvl"] == FireFighter.advanced.value):
-                AdvancedNum += 1
-                #if Volunteeer assigned to a job they're not qualified for
-                if(Vol['role']!="Crew Member"):
-                    print(Vol["volunteer_name"]+ " id "+str(Vol["volunteer_id"])+" not qualified for "+Vol['role'])
-                    return False,Vol["volunteer_name"]+ " id "+str(Vol["volunteer_id"])+" not qualified for "+Vol['role']
+        if(Vol.Explvl == FireFighter.crewleader.value):
+            CrewLeaderNum += 1
 
-            if(Vol["volunteer_explvl"]==FireFighter.crewleader.value):
-                CrewLeaderNum += 1
-
-            if (Vol["volunteer_explvl"] == FireFighter.driver.value):
-                DriverNum += 1
-                # if Volunteeer assigned to a job they're not qualified for
-                if (Vol['role'] == "Crew Leader" or Vol['role'] == "CrewLeader/Driver"):
-                    print(Vol["volunteer_name"] + " id " + str(Vol["volunteer_id"]) + " not qualified for " + Vol[
-                        'role'])
-                    return False,Vol["volunteer_name"] + " id " + str(Vol["volunteer_id"]) + " not qualified for " + Vol[
-                        'role']
+        if (Vol.Explvl == FireFighter.driver.value):
+            DriverNum += 1
+            # if Volunteeer assigned to a job they're not qualified for
+            if (Vol.role == "CrewLeader" or Vol.role == "CrewLeader/Driver"):
+                print(Vol.name + " id " + str(Vol.id) + " not qualified for " + Vol.role)
+                return False,Vol.name + " id " + str(Vol.id) + " not qualified for " + Vol.role
 
 
-        #if theres too many
-        if(Type.TotalReq < TotalNum):
-            print("There are not enough volunteers on "+vehicle["asset_class"].type+" on Request Number "+str(vehicle["asset_id"]))
-            return False,"There are not enough volunteers on "+vehicle["asset_class"].type+" on Request Number "+str(vehicle["asset_id"])
-        # if theres too few
-        if (Type.TotalReq > TotalNum):
-            print("There are too many  volunteers on " + vehicle["asset_class"].type + " on Request Number " + str(
-                vehicle["asset_id"]))
-            return False,"There are too many  volunteers on " + vehicle["asset_class"].type + " on Request Number " + str(
-                vehicle["asset_id"])
-        #checks number of CrewLeaders
-        if (Type.CrewLeaderReq > CrewLeaderNum):
-            print(vehicle["asset_class"].type + " on Request Number " + str(
-                vehicle["asset_id"])+"does not have a CrewLeader")
-            return False, vehicle["asset_class"].type + " on Request Number " + str(
-                vehicle["asset_id"])+"does not have a CrewLeader"
+    #if theres too many
+    if(Type.TotalReq < TotalNum):
+        print("There are not enough volunteers on "+asset_request.AssetType+" on Request Number "+str(asset_request.RequestNo))
+        return False,"There are not enough volunteers on "+asset_request.AssetType+" on Request Number "+str(asset_request.RequestNo)
+    # if theres too few
+    if (Type.TotalReq > TotalNum):
+        print("There are too many  volunteers on " + asset_request.AssetType + " on Request Number " + str(
+            asset_request.RequestNo))
+        return False,"There are too many  volunteers on " + asset_request.AssetType + " on Request Number " + str(
+            asset_request.RequestNo)
+    #checks number of CrewLeaders
+    if (Type.CrewLeaderReq > CrewLeaderNum):
+        print(asset_request.AssetType + " on Request Number " + str(
+            asset_request.RequestNo)+"does not have a CrewLeader")
+        return False, asset_request.AssetType + " on Request Number " + str(
+            asset_request.RequestNo)+"does not have a CrewLeader"
 
-        #Checks Number of drivers
-        if (Type.DriverReq > CrewLeaderNum + DriverNum):
-            print(vehicle["asset_class"].type + " on Request Number " + str(
-                vehicle["asset_id"]) + "does not have a Driver")
-            return False, vehicle["asset_class"].type + " on Request Number " + str(
-                vehicle["asset_id"]) + "does not have a Driver"
-            #  AdvancedReq
-            # TotalReq
-            # self.CrewLeaderReq = CrewLeaderReq
-            # self.DriverReq = DriverReq
+    #Checks Number of drivers
+    if (Type.DriverReq > CrewLeaderNum + DriverNum):
+        print(asset_request.AssetType + " on Request Number " + str(
+            asset_request.RequestNo) + "does not have a Driver")
+        return False, asset_request.AssetType + " on Request Number " + str(
+            asset_request.RequestNo) + "does not have a Driver"
+        #  AdvancedReq
+        # TotalReq
+        # self.CrewLeaderReq = CrewLeaderReq
+        # self.DriverReq = DriverReq
     #If none of hte conditions are breached return True
     print("Valid Assignment")
     return True,"Valid Assignment"
