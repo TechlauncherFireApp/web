d42e33185dc777c577d1a8085c18d6d6f9e6b1b2
Stavros  Dimos <u5825599@anu.edu.au>
2020-09-08 10:04:35 +1000

    Added helper functions, implemented partial fill for invalid models

--- backend/gurobi/Scheduler.py
+++ backend/gurobi/Scheduler.py
@@ -29,10 +29,15 @@ def getDuration(start, end):
 
 def addLightUnitToOutput(seats, currentRequest, Volunteers):
     output = []
+    alreadyAdded = []
     for i in range(2):
         voldict = {}
         role = []
-        voldict["ID"] = Volunteers[seats[i][currentRequest]-1]["ID"]
+        idIndex = seats[i][currentRequest]-1
+        if idIndex < 0 or (idIndex in alreadyAdded):
+            voldict["ID"] = -1
+        else:
+            voldict["ID"] = Volunteers[idIndex]["ID"]
         voldict["positionID"] = i
         if i == 0:
             role.append("driver")
@@ -40,16 +45,23 @@ def addLightUnitToOutput(seats, currentRequest, Volunteers):
         if i == 1:
             role.append("advanced")
         voldict["role"] = role
+        alreadyAdded.append(idIndex)
         output.append(voldict)
+
     return output
 
 
 def addMediumTankerToOutput(seats, currentRequest, Volunteers):
     output = []
+    alreadyAdded = []
     for i in range(3):
         voldict = {}
         role = []
-        voldict["ID"] = Volunteers[seats[i][currentRequest]-1]["ID"]
+        idIndex = seats[i][currentRequest] - 1
+        if idIndex < 0 or (idIndex in alreadyAdded):
+            voldict["ID"] = -1
+        else:
+            voldict["ID"] = Volunteers[idIndex]["ID"]
         voldict["positionID"] = i
         if i == 0:
             role.append("driver")
@@ -58,16 +70,22 @@ def addMediumTankerToOutput(seats, currentRequest, Volunteers):
         if i == 2:
             role.append("basic")
         voldict["role"] = role
+        alreadyAdded.append(idIndex)
         output.append(voldict)
     return output
 
 
 def addHeavyTankerToOutput(seats, currentRequest, Volunteers):
     output = []
+    alreadyAdded = []
     for i in range(5):
         voldict = {}
         role = []
-        voldict["ID"] = Volunteers[seats[i][currentRequest]-1]["ID"]
+        idIndex = seats[i][currentRequest] - 1
+        if idIndex < 0 or (idIndex in alreadyAdded):
+            voldict["ID"] = -1
+        else:
+            voldict["ID"] = Volunteers[idIndex]["ID"]
         voldict["positionID"] = i
         if i == 0:
             role.append("driver")
@@ -78,6 +96,7 @@ def addHeavyTankerToOutput(seats, currentRequest, Volunteers):
         if i == 4:
             role.append("basic")
         voldict["role"] = role
+        alreadyAdded.append(idIndex)
         output.append(voldict)
     return output
 
@@ -117,21 +136,15 @@ for i in range(1):
     AssetRequests.append(assetrequest)
 '''
 
-
-#Takes a list of Volunteers and Requests and returns a volunteer assignment and model
-def Schedule(Volunteers, VehicleRequest):
-
-    #create an array of preferredHours
-    preferredHours = []
-    for volunteer in Volunteers:
-        preferredHours.append(volunteer["prefHours"])
-
+def genShiftLengths(VehicleRequest):
     shiftLengths = []
     for request in VehicleRequest:
         start = request["timeframe"][0]
         end = request["timeframe"][1]
-        shiftLengths.append(getDuration(start,end))
+        shiftLengths.append(getDuration(start, end))
+    return shiftLengths
 
+def genCompatibility(Volunteers, VehicleRequest):
     compatible = []
     for request in VehicleRequest:
         requestRow = []
@@ -139,11 +152,13 @@ def Schedule(Volunteers, VehicleRequest):
         for volunteer in Volunteers:
             temp = False
             for availability in volunteer["availabilities"]:
-                if(rangeSurrounds(availability, requestTime)):
+                if (rangeSurrounds(availability, requestTime)):
                     temp = True
             requestRow.append(temp)
         compatible.append(requestRow)
+    return compatible
 
+def genClashes(VehicleRequest):
     clashing = []
     for request1 in VehicleRequest:
         currentRequestClashes = []
@@ -152,6 +167,28 @@ def Schedule(Volunteers, VehicleRequest):
         clashing.append(currentRequestClashes)
     for i in range(len(VehicleRequest)):
         clashing[i][i] = False
+    return clashing
+
+#Takes a list of Volunteers and Requests and returns a volunteer assignment and model
+def Schedule(Volunteers, VehicleRequest):
+    temp = FullSchedule(Volunteers,VehicleRequest)
+    if temp != []:
+        return temp
+    else:
+        print("Attempting partial fill")
+        return PartialSchedule(Volunteers,VehicleRequest)
+
+def FullSchedule(Volunteers, VehicleRequest):
+    # create an array of preferredHours
+    preferredHours = []
+    for volunteer in Volunteers:
+        preferredHours.append(volunteer["prefHours"])
+
+    shiftLengths = genShiftLengths(VehicleRequest)
+
+    compatible = genCompatibility(Volunteers,VehicleRequest)
+
+    clashing = genClashes(VehicleRequest)
 
     isHeavy = []
     isMedium = []
@@ -201,84 +238,84 @@ def Schedule(Volunteers, VehicleRequest):
         int: S;
         set of int: Volunteers = 1..V;
         set of int: Shifts = 1..S;
-        
+
         array[Shifts,Volunteers] of bool: compatible;
         array[Shifts,Shifts] of bool: clashing;
         array[Shifts,Volunteers] of var bool: assignments;
         array[Volunteers] of int: preferredHours;
         array[Shifts] of int: shiftLength;
-        
+
         array[Shifts] of bool: isHeavy;
         array[Shifts] of bool: isMedium;
         array[Shifts] of bool: isLight;
-        
+
         array[Volunteers] of bool: isBasic;
         array[Volunteers] of bool: isAdvanced;
         array[Volunteers] of bool: isCrewLeader;
         array[Volunteers] of bool: isDriver;
-        
-        array[Shifts] of var Volunteers: seat1;
-        array[Shifts] of var Volunteers: seat2;
-        array[Shifts] of var Volunteers: seat3;
-        array[Shifts] of var Volunteers: seat4;
-        array[Shifts] of var Volunteers: seat5;
-        
+
+        array[Shifts] of var 0..V: seat1;
+        array[Shifts] of var 0..V: seat2;
+        array[Shifts] of var 0..V: seat3;
+        array[Shifts] of var 0..V: seat4;
+        array[Shifts] of var 0..V: seat5;
+
         array[Volunteers,Shifts] of var int: hoursOnAssignment;
         array[Volunteers] of var int: totalHours;
-        
+
         %volunteer availability check
         constraint forall(s in Shifts)(forall(v in Volunteers)(if compatible[s,v] == false then assignments[s,v] == false endif));
-        
+
         %volunteers cannot be assigned to 2 shifts at once
         constraint forall(s1 in Shifts)(forall(s2 in Shifts)(forall(v in Volunteers)(if clashing[s1,s2] /\ assignments[s1,v] then assignments[s2,v] == false endif)));
-        
+
         %all vehicles are filled
         constraint forall(s in Shifts)(if isHeavy[s] then sum(v in Volunteers)(assignments[s,v]) = 5 endif);
         constraint forall(s in Shifts)(if isMedium[s] then sum(v in Volunteers)(assignments[s,v]) = 3 endif);
         constraint forall(s in Shifts)(if isLight[s] then sum(v in Volunteers)(assignments[s,v]) = 2 endif);
-        
+
         %Seat1 Requirements
         constraint forall(s in Shifts)(forall(v in Volunteers) (if seat1[s] == v then assignments[s,v] = true endif));
         constraint forall(s in Shifts)(forall(v in Volunteers)(if seat1[s] == v then isDriver[v] = true endif));
         constraint forall(s in Shifts)(if isLight[s] then forall(v in Volunteers)(if seat1[s] == v then isCrewLeader[v] = true endif) endif);
-        
+
         %seat2 Requirements
         constraint forall(s in Shifts)(forall(v in Volunteers) (if seat2[s] == v then assignments[s,v] = true endif));
         constraint forall(s in Shifts)(if isLight[s] then forall(v in Volunteers)(if seat2[s] == v then isAdvanced[v] = true endif) endif);
         constraint forall(s in Shifts)(if isMedium[s] then forall(v in Volunteers)(if seat2[s] == v then isCrewLeader[v] = true endif) endif);
         constraint forall(s in Shifts)(if isHeavy[s] then forall(v in Volunteers)(if seat2[s] == v then isCrewLeader[v] = true endif) endif);
-        
+
         %seat3 Requirements
         constraint forall(s in Shifts)(forall(v in Volunteers) (if seat3[s] == v then assignments[s,v] = true endif));
         constraint forall(s in Shifts)(if isLight[s] then seat3[s] == seat2[s] endif);
         constraint forall(s in Shifts)(if isMedium[s] then forall(v in Volunteers)(if seat3[s] == v then isBasic[v] = true endif) endif);
         constraint forall(s in Shifts)(if isHeavy[s] then forall(v in Volunteers)(if seat3[s] == v then isAdvanced[v] = true endif) endif);
-        
+
         %seat4 Requirements
         constraint forall(s in Shifts)(forall(v in Volunteers) (if seat4[s] == v then assignments[s,v] = true endif));
         constraint forall(s in Shifts)(if isLight[s] then seat4[s] == seat2[s] endif);
         constraint forall(s in Shifts)(if isMedium[s] then seat4[s] == seat3[s] endif);
         constraint forall(s in Shifts)(if isHeavy[s] then forall(v in Volunteers)(if seat4[s] == v then isAdvanced[v] = true endif) endif);
-        
+
         %seat5 Requirements
         constraint forall(s in Shifts)(forall(v in Volunteers) (if seat5[s] == v then assignments[s,v] = true endif));
         constraint forall(s in Shifts)(if isLight[s] then seat5[s] == seat2[s] endif);
         constraint forall(s in Shifts)(if isMedium[s] then seat5[s] == seat3[s] endif);
         constraint forall(s in Shifts)(if isHeavy[s] then forall(v in Volunteers)(if seat5[s] == v then isBasic[v] = true endif) endif);
-        
+
         %if a volunteer is not on any seats of a shift, they are not on the shift
         constraint forall(s in Shifts)(forall(v in Volunteers) (if seat5[s] != v /\ seat4[s] != v /\ seat3[s] != v /\ seat2[s] != v /\ seat1[s] != v then assignments[s,v] = false endif));
-        
-        
+
+
         %maps shifts to hours worked
         constraint forall(v in Volunteers)(forall(s in Shifts)(if assignments[s,v] then hoursOnAssignment[v,s] = shiftLength[s] else hoursOnAssignment[v,s] = 0 endif));
-        
+
         %defines total hours for each volunteer
         constraint forall(v in Volunteers)(totalHours[v] == sum(s in Shifts)(hoursOnAssignment[v,s]));
-        
+
         %ensures no one is overworked
         constraint forall(v in Volunteers)(totalHours[v] <= preferredHours[v]);
-        
+
         %minimise for the sum of squares difference between preferred work and actual work
         %solve minimize sum(v in Volunteers)((preferredHours[v] - totalHours[v])*(preferredHours[v] - totalHours[v]))
         solve satisfy
@@ -300,7 +337,6 @@ def Schedule(Volunteers, VehicleRequest):
     instance["isCrewLeader"] = isCrewLeader
     instance["isDriver"] = isDriver
     result = instance.solve()
-
     if result.solution:
         output = []
         for i in range(len(VehicleRequest)):
@@ -327,3 +363,186 @@ def Schedule(Volunteers, VehicleRequest):
         output = []
     return output
 
+def PartialSchedule(Volunteers, VehicleRequest):
+    # create an array of preferredHours
+    preferredHours = []
+    for volunteer in Volunteers:
+        preferredHours.append(volunteer["prefHours"])
+
+    shiftLengths = genShiftLengths(VehicleRequest)
+
+    compatible = genCompatibility(Volunteers,VehicleRequest)
+
+    clashing = genClashes(VehicleRequest)
+
+    isHeavy = []
+    isMedium = []
+    isLight = []
+    for request in VehicleRequest:
+        if request["assetClass"] == "heavyTanker":
+            isHeavy.append(True)
+            isMedium.append(False)
+            isLight.append(False)
+        if request["assetClass"] == "mediumTanker":
+            isHeavy.append(False)
+            isMedium.append(True)
+            isLight.append(False)
+        if request["assetClass"] == "lightUnit":
+            isHeavy.append(False)
+            isMedium.append(False)
+            isLight.append(True)
+
+    isDriver = []
+    isCrewLeader = []
+    isAdvanced = []
+    isBasic = []
+    for volunteer in Volunteers:
+        basic = False
+        advanced = False
+        crewLeader = False
+        driver = False
+        for qualification in volunteer["possibleRoles"]:
+            if qualification == "basic":
+                basic = True
+            if qualification == "advanced":
+                advanced = True
+            if qualification == "crewLeader":
+                crewLeader = True
+            if qualification == "driver":
+                driver = True
+        isBasic.append(basic)
+        isAdvanced.append(advanced)
+        isCrewLeader.append(crewLeader)
+        isDriver.append(driver)
+
+    gecode = Solver.lookup("gecode")
+    model = Model()
+    model.add_string(
+        """
+        int: V;
+        int: S;
+        set of int: Volunteers = 1..V;
+        set of int: Shifts = 1..S;
+
+        array[Shifts,Volunteers] of bool: compatible;
+        array[Shifts,Shifts] of bool: clashing;
+        array[Shifts,Volunteers] of var bool: assignments;
+        array[Volunteers] of int: preferredHours;
+        array[Shifts] of int: shiftLength;
+
+        array[Shifts] of bool: isHeavy;
+        array[Shifts] of bool: isMedium;
+        array[Shifts] of bool: isLight;
+
+        array[Volunteers] of bool: isBasic;
+        array[Volunteers] of bool: isAdvanced;
+        array[Volunteers] of bool: isCrewLeader;
+        array[Volunteers] of bool: isDriver;
+
+        array[Shifts] of var Volunteers: seat1;
+        array[Shifts] of var Volunteers: seat2;
+        array[Shifts] of var Volunteers: seat3;
+        array[Shifts] of var Volunteers: seat4;
+        array[Shifts] of var Volunteers: seat5;
+
+        array[Volunteers,Shifts] of var int: hoursOnAssignment;
+        array[Volunteers] of var int: totalHours;
+
+        %volunteer availability check
+        constraint forall(s in Shifts)(forall(v in Volunteers)(if compatible[s,v] == false then assignments[s,v] == false endif));
+
+        %volunteers cannot be assigned to 2 shifts at once
+        constraint forall(s1 in Shifts)(forall(s2 in Shifts)(forall(v in Volunteers)(if clashing[s1,s2] /\ assignments[s1,v] then assignments[s2,v] == false endif)));
+
+        %all vehicles are filled
+        constraint forall(s in Shifts)(if isHeavy[s] then sum(v in Volunteers)(assignments[s,v]) <= 5 endif);
+        constraint forall(s in Shifts)(if isMedium[s] then sum(v in Volunteers)(assignments[s,v]) <= 3 endif);
+        constraint forall(s in Shifts)(if isLight[s] then sum(v in Volunteers)(assignments[s,v]) <= 2 endif);
+
+        %Seat1 Requirements
+        constraint forall(s in Shifts)(forall(v in Volunteers) (if seat1[s] == v then assignments[s,v] = true endif));
+        constraint forall(s in Shifts)(forall(v in Volunteers)(if seat1[s] == v then isDriver[v] = true endif));
+        constraint forall(s in Shifts)(if isLight[s] then forall(v in Volunteers)(if seat1[s] == v then isCrewLeader[v] = true endif) endif);
+
+        %seat2 Requirements
+        constraint forall(s in Shifts)(forall(v in Volunteers) (if seat2[s] == v then assignments[s,v] = true endif));
+        constraint forall(s in Shifts)(if isLight[s] then forall(v in Volunteers)(if seat2[s] == v then isAdvanced[v] = true endif) endif);
+        constraint forall(s in Shifts)(if isMedium[s] then forall(v in Volunteers)(if seat2[s] == v then isCrewLeader[v] = true endif) endif);
+        constraint forall(s in Shifts)(if isHeavy[s] then forall(v in Volunteers)(if seat2[s] == v then isCrewLeader[v] = true endif) endif);
+
+        %seat3 Requirements
+        constraint forall(s in Shifts)(forall(v in Volunteers) (if seat3[s] == v then assignments[s,v] = true endif));
+        constraint forall(s in Shifts)(if isLight[s] then seat3[s] == seat2[s] endif);
+        constraint forall(s in Shifts)(if isMedium[s] then forall(v in Volunteers)(if seat3[s] == v then isBasic[v] = true endif) endif);
+        constraint forall(s in Shifts)(if isHeavy[s] then forall(v in Volunteers)(if seat3[s] == v then isAdvanced[v] = true endif) endif);
+
+        %seat4 Requirements
+        constraint forall(s in Shifts)(forall(v in Volunteers) (if seat4[s] == v then assignments[s,v] = true endif));
+        constraint forall(s in Shifts)(if isLight[s] then seat4[s] == seat2[s] endif);
+        constraint forall(s in Shifts)(if isMedium[s] then seat4[s] == seat3[s] endif);
+        constraint forall(s in Shifts)(if isHeavy[s] then forall(v in Volunteers)(if seat4[s] == v then isAdvanced[v] = true endif) endif);
+
+        %seat5 Requirements
+        constraint forall(s in Shifts)(forall(v in Volunteers) (if seat5[s] == v then assignments[s,v] = true endif));
+        constraint forall(s in Shifts)(if isLight[s] then seat5[s] == seat2[s] endif);
+        constraint forall(s in Shifts)(if isMedium[s] then seat5[s] == seat3[s] endif);
+        constraint forall(s in Shifts)(if isHeavy[s] then forall(v in Volunteers)(if seat5[s] == v then isBasic[v] = true endif) endif);
+
+        %if a volunteer is not on any seats of a shift, they are not on the shift
+        constraint forall(s in Shifts)(forall(v in Volunteers) (if seat5[s] != v /\ seat4[s] != v /\ seat3[s] != v /\ seat2[s] != v /\ seat1[s] != v then assignments[s,v] = false endif));
+
+
+        %maps shifts to hours worked
+        constraint forall(v in Volunteers)(forall(s in Shifts)(if assignments[s,v] then hoursOnAssignment[v,s] = shiftLength[s] else hoursOnAssignment[v,s] = 0 endif));
+
+        %defines total hours for each volunteer
+        constraint forall(v in Volunteers)(totalHours[v] == sum(s in Shifts)(hoursOnAssignment[v,s]));
+
+        %ensures no one is overworked
+        constraint forall(v in Volunteers)(totalHours[v] <= preferredHours[v]);
+
+        %maximise for number of volunteers assigned
+        solve maximize sum(s in Shifts)(sum(v in Volunteers)(assignments[s,v]))
+        """
+    )
+
+    instance = Instance(gecode, model)
+    instance["V"] = len(Volunteers)
+    instance["S"] = len(VehicleRequest)
+    instance["preferredHours"] = preferredHours
+    instance["shiftLength"] = shiftLengths
+    instance["compatible"] = compatible
+    instance["clashing"] = clashing
+    instance["isHeavy"] = isHeavy
+    instance["isMedium"] = isMedium
+    instance["isLight"] = isLight
+    instance["isBasic"] = isBasic
+    instance["isAdvanced"] = isAdvanced
+    instance["isCrewLeader"] = isCrewLeader
+    instance["isDriver"] = isDriver
+    result = instance.solve()
+    if result.solution:
+        output = []
+        for i in range(len(VehicleRequest)):
+            vehicledict = {}
+            vehicledict["shiftID"] = VehicleRequest[i]["shiftID"]
+            vehicledict["assetClass"] = VehicleRequest[i]["assetClass"]
+            vehicledict["timeframe"] = VehicleRequest[i]["timeframe"]
+            seats = []
+            seats.append(result["seat1"])
+            seats.append(result["seat2"])
+            seats.append(result["seat3"])
+            seats.append(result["seat4"])
+            seats.append(result["seat5"])
+            if vehicledict["assetClass"] == "lightUnit":
+                volunteers = addLightUnitToOutput(seats, i, Volunteers)
+            if vehicledict["assetClass"] == "mediumTanker":
+                volunteers = addMediumTankerToOutput(seats, i, Volunteers)
+            if vehicledict["assetClass"] == "heavyTanker":
+                volunteers = addHeavyTankerToOutput(seats, i, Volunteers)
+            vehicledict["volunteers"] = volunteers
+            output.append(vehicledict)
+    else:
+        print("No partial fill possible")
+        output = []
+    return output
