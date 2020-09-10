7d6c94615df942f717d1c7ce7b05a134b13d2ce0
caleb <u6373233@anu.edu.au>
2020-09-10 12:02:02 +1000

    modified display to handle unassigned positions in the initial request. For an unassigned position i on an asset j, we should set assetRequest[j].volunteers[i].ID = empty string.

--- ui/src/AssetRequestVolunteers/assetRequestVolunteers.tsx
+++ ui/src/AssetRequestVolunteers/assetRequestVolunteers.tsx
@@ -25,7 +25,7 @@ interface volunteer {
 interface Position {
   positionID: number;
   ID: string;
-  volunteer: volunteer; //not to be saved in database
+  volunteer?: volunteer; //not to be saved in database
   role: string[];
 }
 
@@ -71,10 +71,16 @@ export default class AssetRequestVolunteers extends React.Component<any, State> 
     for (const asset of output) {
       for (const position of asset.volunteers) {
         //find the corresponding volunteer
-        for (let j = 0; j < volunteerList.length; j++) {
-          if (volunteerList[j].ID === position.ID) {
-            position.volunteer = { ...volunteerList[j] };
-            j = volunteerList.length;
+
+        if (position.ID === "") {
+          //empty position
+          position.volunteer = undefined;
+        } else {
+          for (let j = 0; j < volunteerList.length; j++) {
+            if (volunteerList[j].ID === position.ID) {
+              position.volunteer = { ...volunteerList[j] };
+              j = volunteerList.length;
+            }
           }
         }
       }
@@ -99,11 +105,11 @@ export default class AssetRequestVolunteers extends React.Component<any, State> 
     }).then((res: AxiosResponse): void => {
       let tmp = res.data["results"]
       for (const v of tmp) {
-        let convertedAvailabilities : any = [];
+        let convertedAvailabilities: any = [];
         for (const a of v.availabilities) {
           const start = new Date(Date.parse(a[0]));
           const end = new Date(Date.parse(a[1]));
-          convertedAvailabilities.push({startTime: start, endTime: end});
+          convertedAvailabilities.push({ startTime: start, endTime: end });
         }
         v.availabilities = convertedAvailabilities;
       }
@@ -183,128 +189,6 @@ export default class AssetRequestVolunteers extends React.Component<any, State> 
     }
   }
 
-  // TESTING FUNCTION
-  getTestData = (): void => {
-    //   let assetRequest: any = [];
-    //   let volunteerList: any = [];
-    //   let now: Date = new Date();
-
-    //   const asset1: any = {
-    //     shiftID: 1,
-    //     assetClass: "Light Unit",
-    //     startTime: new Date(2020, 5, 10, 10),
-    //     endTime: new Date(2020, 5, 10, 16),
-    //     volunteers: [{
-    //       positionID: 0,
-    //       volunteerID: "1",
-    //       roles: ["Driver", "Crew Leader"]
-    //     },
-    //     {
-    //       positionID: 1,
-    //       volunteerID: "2",
-    //       roles: ["Crew Member"]
-    //     }
-    //     ]
-    //   }
-    //   const asset2: any = {
-    //     shiftID: 2,
-    //     assetClass: "Light Unit",
-    //     startTime: now,
-    //     endTime: now,
-    //     volunteers: [{
-    //       positionID: 0,
-    //       volunteerID: "3",
-    //       roles: ["Driver", "Crew Leader"]
-    //     },
-    //     {
-    //       positionID: 1,
-    //       volunteerID: "4",
-    //       roles: ["Crew Member"]
-    //     }
-    //     ]
-    //   }
-    //   const recommendation: any[] = [asset1, asset2];
-
-    //   const vol1: volunteer = {
-    //     id: "1",
-    //     firstName: "Caleb",
-    //     lastName: "Addison",
-    //     email: "caleb.blah@blah.com",
-    //     mobileNo: "0412490340",
-    //     prefHours: 10,
-    //     possibleRoles: ["Driver", "Crew Leader", "Crew Member"],
-    //     qualifications: ["heavy rigid license", "pump training", "crew leader training", "advanced training",],
-    //     availability: [{ startTime: new Date(2020, 5, 10, 8), endTime: new Date(2020, 5, 10, 12) },
-    //     { startTime: new Date(2020, 5, 10, 15), endTime: new Date(2020, 5, 10, 20) }]
-    //   };
-    //   const vol2: volunteer = {
-    //     id: "2",
-    //     firstName: "Tom",
-    //     lastName: "Willis",
-    //     email: "tom.blah@blah.com",
-    //     mobileNo: "0411222333",
-    //     prefHours: 15,
-    //     possibleRoles: ["Crew Member"],
-    //     qualifications: ["advanced training"],
-    //     availability: [{ startTime: new Date(2022, 1, 1), endTime: new Date(2022, 2, 1) }]
-    //   };
-    //   const vol3: volunteer = {
-    //     id: "3",
-    //     firstName: "Amandeep",
-    //     lastName: "Singh",
-    //     email: "aman.blah@blah.com",
-    //     mobileNo: "1234567890",
-    //     prefHours: 8,
-    //     possibleRoles: ["Driver", "Crew Leader", "Crew Member"],
-    //     qualifications: ["heavy rigid license", "pump training", "crew leader training", "advanced training",],
-    //     availability: [{ startTime: new Date(1990, 1, 1, 1, 1, 1, 1), endTime: new Date(1991, 1, 1, 1, 1, 1, 1) }]
-    //   };
-    //   const vol4: volunteer = {
-    //     id: "4",
-    //     firstName: "Stavros",
-    //     lastName: "Dimos",
-    //     email: "divos.blah@blah.com",
-    //     mobileNo: "9876543120",
-    //     prefHours: 20,
-    //     possibleRoles: ["Driver", "Crew Leader", "Crew Member"],
-    //     qualifications: ["heavy rigid license", "pump training", "crew leader training", "advanced training",],
-    //     availability: [{ startTime: new Date(1990, 1, 1, 1, 1, 1, 1), endTime: new Date(1991, 1, 1, 1, 1, 1, 1) },
-    //     { startTime: new Date(2020, 5, 10, 8), endTime: new Date(2020, 5, 10, 20) }]
-    //   };
-    //   const vol5: volunteer = {
-    //     id: "5",
-    //     firstName: "Cyrus",
-    //     lastName: "Safdsar",
-    //     email: "cyrus.blah@blah.com",
-    //     mobileNo: "541234345",
-    //     prefHours: 7,
-    //     possibleRoles: ["Crew Member"],
-    //     qualifications: ["advanced training",],
-    //     availability: [{ startTime: new Date(2020, 1, 1, 1, 1, 1, 1), endTime: new Date(2021, 1, 1, 1, 1, 1, 1) }]
-    //   };
-    //   const vol6: volunteer = {
-    //     id: "6",
-    //     firstName: "Charles",
-    //     lastName: "Luchetti",
-    //     email: "charles.blah@blah.com",
-    //     mobileNo: "123451234",
-    //     prefHours: 14,
-    //     possibleRoles: ["Driver", "Crew Member"],
-    //     qualifications: ["heavy rigid license", "pump training", "advanced training",],
-    //     availability: [{ startTime: new Date(2020, 1, 1, 1, 1, 1, 1), endTime: new Date(2021, 1, 1, 1, 1, 1, 1) }]
-    //   };
-    //   volunteerList.push(vol1);
-    //   volunteerList.push(vol2);
-    //   volunteerList.push(vol3);
-    //   volunteerList.push(vol4);
-    //   volunteerList.push(vol5);
-    //   volunteerList.push(vol6);
-
-    //   assetRequest = this.mapVolunteersToRequest(recommendation, volunteerList);
-    //   const assignedVolunteers = this.identifyAssignedVolunteers(assetRequest);
-    //   this.setState({ assetRequest, volunteerList, assignedVolunteers })
-  }
-
   submitData(): void {
     //TODO need aman's help with this function (this is 1.3.8)
     //need to save the 'assetRequest' list in the database (IGNORING the volunteer field in each list of volunteers, i.e. only store the ID not all volunteer data)
@@ -369,7 +253,7 @@ export default class AssetRequestVolunteers extends React.Component<any, State> 
     let map: Map<string, { shiftID: number, positionID: number }> = new Map();
     assetRequest.map((a: asset) => {
       a.volunteers.map((p: Position) => {
-        let v: volunteer = p.volunteer;
+        let v: (volunteer | undefined) = p.volunteer;
         if (!(typeof v === 'undefined')) {
           map.set(v.ID, { shiftID: a.shiftID, positionID: p.positionID })
         }
