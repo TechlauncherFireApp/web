25a7d4686a3871dd64e595884c437364373f6b93
Cyrus <u6858500@anu.edu.au>
2020-09-27 22:53:18 +1000

    The saved requests are displayed

--- ui/src/ViewExistingRequests/existingRequestSelector.tsx
+++ ui/src/ViewExistingRequests/existingRequestSelector.tsx
@@ -1,35 +1,96 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
+import axios, { AxiosResponse, AxiosError } from "axios";
 
-
-// just a dummy state for now, implement however desired
-interface State {
-    selectedRequest: any;
-    selectedID: string;
-    allRequests: any;
+interface AssetRequest {
+  id: string;
+  title: string;
 }
 
-export default class ExistingRequestSelector extends React.Component<any, State> {
-    state = {
-        selectedRequest: undefined,
-        selectedID: "",
-        allRequests: undefined,
-    };
 
-    getRequest = (): void => {
-        /* to view a request already existing in the database, simply call the below line window.open
-           with 'id' replaced with the id of the selected request. */
-        const id = "2a8a0f8767364af"
-        window.open(window.location.origin + `/assetRequest/volunteers/${id}`, "_self", "", false);
-    }
 
-    render() {
-        return (
-            <div className="padding">
-                <h4>View Existing Requests</h4>
-                <hr />
-                <button className="type-1" onClick={this.getRequest}>View: 2a8a0f8767364af</button>
-            </div>
-        );
+interface State {
+  requests: AssetRequest[];
+  selectedRequest?: AssetRequest;
+}
+
+export default class existingRequestSelector extends React.Component<any, State> {
+
+  state: State = {
+    requests: [],
+    selectedRequest: undefined
+  };
+  select_request: React.RefObject<HTMLSelectElement>;
+
+
+  constructor(props: any) {
+    super(props);
+    this.select_request = React.createRef();
+  }
+  getRequest = (): void => {
+    /* to view a request already existing in the database, simply call the below line window.open
+       with 'id' replaced with the id of the selected request. */
+    const id = "2a8a0f8767364af"
+    window.open(window.location.origin + `/assetRequest/volunteers/${id}`, "_self", "", false);
+}
+  componentDidMount(): void {
+    let l: AssetRequest[] = [];
+    axios.request({
+      url: "existing_requests",
+      baseURL: "http://localhost:5000/",
+      method: "GET",
+      timeout: 15000,
+      // withCredentials: true,
+      headers: { "X-Requested-With": "XMLHttpRequest" }
+    }).then((res: AxiosResponse): void => {
+      l = res.data["results"];
+
+
+      console.log("Data from backend returned:")
+      console.log(l);
+
+      this.setState({ requests: l })
+    }).catch((err: AxiosError): void => {
+      alert(err.message);
+    });
+  }
+
+  test = (): void => {
+
+    let ID: string = this.select_request.current ? this.select_request.current.value : "";
+
+    //find the volunteer we selected from the list
+    let v: (AssetRequest | undefined) = undefined;
+    for (let i = 0; i < this.state.requests.length; i++) {
+      if (this.state.requests[i].id === ID) {
+        v = this.state.requests[i];
+        i = this.state.requests.length;
+      }
     }
+    this.setState({ selectedRequest: v }, () => { this.loadRequest() });
+  }
+
+  loadRequest = (): void => {
+    if (this.state.selectedRequest === undefined) {
+      console.log("no request selected");
+    } else {
+      window.open(window.location.origin + `/assetRequest/volunteers/${this.state.selectedRequest.id}`, "_self", "", false);
+    }
+  }
+
+  render() {
+    return (
+      <div className="padding">
+        <h4>Saved Requests</h4>
+        <hr />
+        <p>Select a saved request to see further information.</p>
+        <select ref={this.select_request}>
+          <option value="" hidden selected>Select saved request</option>
+          {this.state.requests.map((v: AssetRequest) =>
+            <option value={v.title}>{v.title}</option>
+          )}
+        </select>
+        <button className="type-1 margin" onClick={this.test}>View Assignment</button>
+      </div>
+    );
+  }
 }
