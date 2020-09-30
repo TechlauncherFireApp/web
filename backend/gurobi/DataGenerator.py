3fc43d2067c7fa615bf4d2c4e73d817bb9d81a5f
Cyrus <u6858500@anu.edu.au>
2020-09-30 15:23:18 +1000

    Cleaned up the backend

--- backend/gurobi/DataGenerator.py
+++ backend/gurobi/DataGenerator.py
@@ -5,10 +5,6 @@ import datetime
 from backend.gurobi.AssetTypes import *
 
 # returns a list populated with the the hours in a week to be scheduled
-from backend.gurobi.Names import *
-
-
-
 def shiftpopulator():
     results = []
     # weeknumber can be added if need be by adding an extra forloop and the code could be very similar to the hours
@@ -20,11 +16,6 @@ def shiftpopulator():
             results.append(i + str(j))
     return results
 
-def NumberGenerator():
-    tempnumber = "04"
-    for j in range(8):
-        tempnumber += str(random.randint(0, 9))
-    return tempnumber
 
 
 #turns shift strings into timeblocks
@@ -112,15 +103,13 @@ def AvailabilityGenerator():
 
     return AvailabilityConverter(AvailDict)
 
-QualificationList=["Heavy Rigid Vehicle License","Tanker Driving training",
-                   "Urgent Duty Driving Training","Advanced Pumping Skills","Crew Leader Course",
-                   "Advanced Firefighting Qualification"]
+
 
 def next_monday():
     i=0
     while((datetime.datetime.today()+datetime.timedelta(days=i)).weekday()!=0):
         i+=1
-    return  datetime.datetime.today()+ datetime.timedelta(days=i)
+    return  (datetime.datetime.today()+ datetime.timedelta(days=i)).replace(hour=0,minute=0,second=0,microsecond=0)
 
 
 
@@ -131,7 +120,6 @@ def AvailabilityConverter(AvailabilityDict):
     Days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
     #the
     MondayTime=next_monday()
-    MondayTime=MondayTime.replace(hour=0,minute=0,second=0,microsecond=0)
     #this is a boolean that tracks the start of a shift in order to convert into a set of tuples
     #with a start and end
     NewShiftCheck=False
@@ -154,92 +142,7 @@ def AvailabilityConverter(AvailabilityDict):
             timepairs=[]
     return TimePairList
 
-# each volunteer has an Name,Experience level, preferred Hours, Availability
-# is availability different
-class Volunteer:
-    def __init__(self, id,name, Explvl, prefHours,phonenumber,Availability,Qualifications,YearsOfExperience):
-        self.id=id
-        self.name = name
-        self.Explvl = Explvl
-        self.prefHours = prefHours
-        self.phonenumber=phonenumber
-        self.Availability = Availability
-        self.Qualifications=Qualifications
-        self.YearsOfExperience=YearsOfExperience
 
-#Randomly generating a group of different Volunteers
-#THIS FUNCTION OCCASIONALLY ATTEMPTS TO ACCESS AN OUT OF BOUNDS INDEX, around line 110
-def VolunteerGenerate(volunteerNo, folder_path):
-    list_volunteers = []
-    #generates twice as many advanced firefighters as basic
-    for i in range(volunteerNo):
-        QualDict={}
-        #Creates a dictionary with all the qualifications as false
-        for j in QualificationList:
-            QualDict[j]=False
-        #Generates a random name from the pool available
-        Name=firstNames[random.randint(0,len(firstNames)-1)]+" "+lastNames[random.randint(0,len(lastNames)-1)]
-        #50% are basic
-        if(booleangenerator(50)):
-            years=random.randint(0,2)
-
-        #30% are just advanced
-        elif(booleangenerator(60)):
-            years = random.randint(3, 10)
-
-            #corresponds to the advanced firefighting qualifications
-            QualDict[QualificationList[5]]=True
-        #14% are drivers
-        elif(booleangenerator(70)):
-            years = random.randint(7, 20)
-            for j in range(len(QualificationList)):
-                QualDict[QualificationList[j]]=True
-        #6% are crewleaders
-        else:
-            years = random.randint(4, 15)
-            for j in range(len(QualificationList)):
-                #Corresponds to the Advanced firefighter qualification and the Crew Leader course
-                if(j>=4):
-                    QualDict[QualificationList[j]]=True
-        VolunteerQualificationList=[]
-        for k in QualificationList:
-            if (QualDict[k] == True):
-               VolunteerQualificationList.append(k)
-
-        exp=QualificationtoRoleqaulification(years,QualDict)
-        #preferred hours between 6 and 14
-        prefnum=random.randint(6, 14)
-        #Generates an Availability
-        AvailDict = AvailabilityGenerator()
-
-
-        #generates a random australian phone number
-        tempnumber = NumberGenerator()
-        #adds the volunteer to the list with all the generated data
-        list_volunteers.append(Volunteer(i,Name, exp,prefnum,tempnumber,AvailDict,VolunteerQualificationList,years))
-    VolunteerJson(list_volunteers,folder_path)
-    return list_volunteers
-
-
-
-def VolunteerTest(number):
-    Volunteers=VolunteerGenerate(number,"Volunteers")
-    for i in Volunteers:
-            print("ID: " + str(i.id))
-            print("Name: "+ i.name)
-            print("preferred Hours: "+str(i.prefHours))
-            print("Experience level: "+str(i.Explvl))
-            print("Phone Number: "+i.phonenumber)
-            print("Years of Experience: "+str(i.YearsOfExperience))
-            print("Additional Qualifications: ")
-            for k in i.Qualifications:
-                    print(k)
-            print("Availability: ")
-            for j in shiftpopulator():
-                print(j+": "+str(i.Availability[j]))
-            print("\n")
-
-print(AvailabilityGenerator())
 def deleteContents(path):
     import os, shutil
     folder = path
@@ -266,49 +169,3 @@ def VolunteerJson(Volunteers, folder_path):
             json.dump(i.__dict__, fp)
         j += 1
 
-# Load a single volunteer by file name
-def LoadVolunteerFile(folder_path, file_name):
-    try:
-        file_path = os.path.join(folder_path, file_name)
-        with open(file_path, 'r') as f:
-            contents = json.load(f)
-            # compile their details into a Volunteer object
-            file_volunteer = Volunteer(
-                contents['id'],
-                contents['name'],
-                contents['Explvl'],
-                contents['prefHours'],
-                contents['phonenumber'],
-                contents['Availability'],
-                contents['Qualifications'],
-                contents['YearsOfExperience'],
-            )
-            return file_volunteer
-    except:
-        print("Could not open file: {}".format(str(folder_path) + str(file_name)))
-
-# Load every volunteer. Return a list of all these Volunteer objects
-def LoadVolunteers(folder_path):
-    list_volunteers = []
-    # Get all files in path
-    for file_name in os.listdir(folder_path):
-        list_volunteers.append(LoadVolunteerFile(folder_path, file_name))
-    return list_volunteers
-
-# Load a single volunteer by id
-def LoadVolunteer(folder_path, id):
-    file_name = 'volunteer'+str(id)+'.json'
-    return LoadVolunteerFile(folder_path, file_name)
-    
-
-# Ensure the correct number of volunteers have been generated
-# If regenerate is True, force generate new volunteers
-def SetVolunteerNumber(folder_path, number, regenerate):
-    if regenerate:
-        VolunteerGenerate(number, folder_path)
-        print("Generated new volunteers")
-    else:
-        number_volunteers = len(os.listdir(folder_path))
-        if number_volunteers is not number:
-            VolunteerGenerate(number, folder_path)
-            print("Generated new volunteers")
