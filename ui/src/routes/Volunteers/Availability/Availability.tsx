43b8dc7b9c6e0d35b9058f5897faf16c0c68cd7c
dylan <dylanhouse@hotmail.com.au>
2021-04-12 14:26:44 +1000

    UI Complete

--- ui/src/routes/Volunteers/Availability/Availability.tsx
+++ ui/src/routes/Volunteers/Availability/Availability.tsx
@@ -3,89 +3,142 @@ import './Availability.scss';
 import axios, {AxiosResponse} from 'axios';
 import {backendPath} from '../../../config';
 import DayPicker, {DateUtils} from "react-day-picker";
-import "react-day-picker/lib/style.css";
+// @ts-ignore
+import TimeRange from 'react-timeline-range-slider';
+import {endOfToday, set} from 'date-fns';
+
+
+const now = new Date()
+const getTodayAtSpecificHour = (hour = 12) =>
+    set(now, {hours: hour, minutes: 0, seconds: 0, milliseconds: 0})
+
+const selectedStart = getTodayAtSpecificHour()
+const selectedEnd = getTodayAtSpecificHour()
+
+const startTime = getTodayAtSpecificHour(0)
+const endTime = endOfToday()
 
 interface State {
   // Preferred Date and Time
   selectedDays: Date[];
 
   allow_getPrefHours: boolean;
+
+  // State referring to TimeRange
+  error: boolean;
+  selectedInterval: any;
 }
 
-export default class Availability extends React.Component<any, State> {
+export default class Availability extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
     this.handleDayClick = this.handleDayClick.bind(this);
     this.state = {
       selectedDays: [],
       allow_getPrefHours: true,
+      error: false,
+      selectedInterval: [selectedStart, selectedEnd],
     }
   }
 
-  // Component Methods
+  // Calendar Methods
+
   // @ts-ignore
-  handleDayClick(day, { selected }) {
+  handleDayClick(day, {selected}) {
     const selectedDays = this.state.selectedDays.concat();
     if (selected) {
       const selectedIndex = selectedDays.findIndex((selectedDay: Date) =>
-        DateUtils.isSameDay(selectedDay, day)
+          DateUtils.isSameDay(selectedDay, day)
       );
       selectedDays.splice(selectedIndex, 1);
     } else {
       selectedDays.push(day);
     }
-    this.setState({ selectedDays });
+    this.setState({selectedDays});
   }
 
+  // TimeRange Methods
 
-  // Backend Requests
-    getPrefDates(): void {
-    if (!this.state.allow_getPrefHours) return;
-    this.setState({ allow_getPrefHours: false });
+  // @ts-ignore
+  errorHandler = ({error}) => this.setState({error});
 
-      axios
-      .request({
-        url: backendPath + '/volunteer/prefhours',
-        method: 'GET',
-        params: { volunteerID: this.props.match.params.id },
-        headers: {
-          Authorization: 'Bearer ' + localStorage.getItem('access_token'),
-        },
-      })
-      .then((res: AxiosResponse): void => {
-
-      })
-
+  onChangeCallback = (selectedInterval: any) => {
+    this.setState({selectedInterval});
   };
 
-    exit(): void {
-      window.open(
+  deleteAvailability = (): void => {
+
+  }
+
+  // Backend Requests
+  getPrefDates(): void {
+    if (!this.state.allow_getPrefHours) return;
+    this.setState({allow_getPrefHours: false});
+
+    axios
+        .request({
+          url: backendPath + '/volunteer/prefhours',
+          method: 'GET',
+          params: {volunteerID: this.props.match.params.id},
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+          },
+        })
+        .then((res: AxiosResponse): void => {
+
+        })
+  };
+
+  patchSchedule = (): void => {
+
+  }
+
+  exit = (): void => {
+    window.open(
         `${window.location.origin}/volunteer/${this.props.match.params.id}`,
-      '_self',
-      '',
-      false)
-    }
+        '_self',
+        '',
+        false)
+  }
 
   render() {
+    const {selectedInterval, error} = this.state;
     return (
-        <div>
-          <div>
-            <DayPicker
-                showOutsideDays
-                showWeekNumbers
-                selectedDays={this.state.selectedDays}
-                // @ts-ignore
-                onDayClick={this.handleDayClick}
-                fromMonth={new Date()}
-                todayButton="Return to today"
+        <availability>
+          <div className="exterior">
+            <div className="calendar">
+              <DayPicker
+                  showOutsideDays
+                  showWeekNumbers
+                  selectedDays={this.state.selectedDays}
+                  // @ts-ignore
+                  onDayClick={this.handleDayClick}
+                  fromMonth={new Date()}
+                  todayButton="Return to today"
+              />
+            </div>
+            <div className="time-range">
+            <TimeRange
+                error={error}
+                selectedInterval={selectedInterval}
+                timelineInterval={[startTime, endTime]}
+                onUpdateCallback={this.errorHandler}
+                onChangeCallback={this.onChangeCallback}
             />
+            </div>
+            <div className="con">
+              <button className="type-1" onClick={this.patchSchedule}>
+                Save Shifts
+              </button>
+              <button className="type-3" onClick={this.deleteAvailability}>
+                Delete Shift
+              </button>
+              <button className="type-2" onClick={this.exit}>
+                Return
+              </button>
+            </div>
           </div>
-
-          <div>
-            <button className={"type-1"} onClick={(): void => this.exit()}>
-              Return
-            </button>
-          </div>
-        </div>
-  )};
+        </availability>
+    )
+  };
 }
