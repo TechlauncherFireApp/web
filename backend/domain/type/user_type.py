ad3d2a37f2f662ce60794586679bb491309c2893
dylan <dylanhouse@hotmail.com.au>
2021-08-23 16:24:36 +1000

    Attemp alembic revision

--- backend/domain/type/user_type.py
+++ backend/domain/type/user_type.py
@@ -4,7 +4,4 @@ from enum import Enum
 class UserType(Enum):
     VOLUNTEER = 0
     ADMIN = 1
-    # promotion 0 -> 1 -> 2
-    # demotion 1 -> 0
-    # self-demotion 2 -> 1 : Just an idea from Dylan, feel free to remove this.
     ROOT_ADMIN = 2
