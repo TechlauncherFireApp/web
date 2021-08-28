21b5756860c023083188e873bd5748f6e6abbf1a
Jasmeen Kaur <u6871990@anu.edu.au>
2021-08-28 16:24:19 +1000

    Modified login page and included the image

--- ui/src/routes/Authentication/login.js
+++ ui/src/routes/Authentication/login.js
@@ -5,6 +5,7 @@ import { useLocation } from 'react-router';
 import { NavLink, useHistory } from 'react-router-dom';
 
 import { backendPath } from '../../config';
+import ff_image from './fire-fighters.jpg';
 
 function Login() {
   const [values, setValues] = useState({});
@@ -43,54 +44,60 @@ function Login() {
   }
 
   return (
-    <div className="padding">
-      {showRegistrationSuccess && error === undefined && (
-        <div className="w-50 ml-auto mr-auto alert alert-success" role="alert">
-          Registration successful, you may now login.
-        </div>
-      )}
-      <form
-        onSubmit={submit}
-        className={'mt6 w-50 w-10 mr-auto ba br2 b--black-10 pa3'}>   {/*FIXME -- jasmeen*/}
-        {/*className={'mt6 w-50 ml-auto mr-auto ba br2 b--black-10 pa3'}>*/}
-        <h2>Login</h2>
-        <div className="form-group">
-          <label htmlFor={'email'}>Email*:</label>
-          <input
-            className={'form-control'}
-            type="text"
-            name="email"
-            onChange={(e) => {
-              handleChange('email', e.target.value);
-            }}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor={'password'}>Password*:</label>
-          <input
-            className={'form-control'}
-            type="password"
-            name="password"
-            onChange={(e) => {
-              handleChange('password', e.target.value);
-            }}
-          />
-        </div>
-        <input
-          type="submit"
-          value="Submit"
-          className={'btn bg-light-red pv2 ph3 br2 b near-white dim'}
-        />
-        <div className={'mt2'}>
-          <NavLink to={'/register'}>Register</NavLink>
-        </div>
-        {error && (
-          <div className="alert alert-danger" role="alert">
-            {error}
+    <>
+      <div className="padding1">
+        {showRegistrationSuccess && error === undefined && (
+          <div className="w-50 ml-auto mr-auto alert alert-success" role="alert">
+            Registration successful, you may now login.
           </div>
         )}
-      </form>
-    </div>
+        <form
+          onSubmit={submit}
+          className={'mt1 mb1 ml1 mr1 ba br2 b--black-10 pa3'}>   {/*FIXME -- change1 - jasmeen*/}
+          {/*className={'mt6 w-50 ml-auto mr-auto ba br2 b--black-10 pa3'}>*/}
+          {/*top-margin bottom-margin left-margin right-margin border-style border-radius border-color padding*/}
+          <h2>Login</h2>
+          <div className="form-group">
+            <label htmlFor={'email'}>Email*:</label>
+            <input
+              className={'form-control'}
+              type="text"
+              name="email"
+              onChange={(e) => {
+                handleChange('email', e.target.value);
+              }}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor={'password'}>Password*:</label>
+            <input
+              className={'form-control'}
+              type="password"
+              name="password"
+              onChange={(e) => {
+                handleChange('password', e.target.value);
+              }}
+            />
+          </div>
+          <input
+            type="submit"
+            value="Submit"
+            className={'btn bg-light-red pv2 ph3 br2 b near-white dim'}
+          />
+          <div className={'mt2'}>
+            <NavLink to={'/register'}>Register</NavLink>
+          </div>
+          {error && (
+            <div className="alert alert-danger" role="alert">
+              {error}
+            </div>
+          )}
+        </form>
+      </div>
+      <div id="login-ff-image" align="top">
+        <img src={ff_image} alt="Fire fighters" align={"right"}/>
+      </div>
+   </>
   );
 }
 
