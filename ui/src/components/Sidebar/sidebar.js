cf64caaf81f08c5799b65793b1f95e1e46a37734
Phillip La <la.TRN@infosys.com>
2022-03-21 17:46:00 +1100

    Implement proper spacing of menu items in sidebar

--- ui/src/components/Sidebar/sidebar.js
+++ ui/src/components/Sidebar/sidebar.js
@@ -18,21 +18,44 @@ const Sidebar = () => {
                 <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>Menu</CDBSidebarHeader>
                 <CDBSidebarContent>
                     <CDBSidebarMenu>
-                        <a href='https://esa.act.gov.au/cbr-be-emergency-ready/bushfires/bushfire-ready'>
-                            <CDBSidebarMenuItem>Bushfire ready</CDBSidebarMenuItem>
-                        </a>
-                        <a href='https://esa.act.gov.au/be-emergency-ready/extreme-heat'>
-                            <CDBSidebarMenuItem>Extreme heat</CDBSidebarMenuItem>
-                        </a>
-                        <a href='https://esa.act.gov.au/be-emergency-ready/fire-safety'>
-                            <CDBSidebarMenuItem>Total fire bans</CDBSidebarMenuItem>
-                        </a>
-                        <a href='https://esa.act.gov.au/cbr-be-emergency-ready/emergency-arrangements'>
-                            <CDBSidebarMenuItem>Emergency</CDBSidebarMenuItem>
-                        </a>
+                        <div>
+                            <CDBSidebarMenuItem>
+                                <h1 className='submenu-heading'>Resources</h1>
+                            </CDBSidebarMenuItem>
+                        </div>
+                        <ul className='sidebar-list'>
+                            <li>
+                                <a href='https://esa.act.gov.au/cbr-be-emergency-ready/bushfires/bushfire-ready' target='none' rel='noopener noreferrer'>
+                                    <CDBSidebarMenuItem>Bushfire ready</CDBSidebarMenuItem>
+                                </a>
+                            </li>
+                            <li>
+                                <a href='https://esa.act.gov.au/be-emergency-ready/extreme-heat' target='none' rel='noopener noreferrer'>
+                                    <CDBSidebarMenuItem>Extreme heat</CDBSidebarMenuItem>
+                                </a>
+                            </li>
+                            <li>
+                                <a href='https://esa.act.gov.au/be-emergency-ready/fire-safety' target='none' rel='noopener noreferrer'>
+                                    <CDBSidebarMenuItem>Total fire bans</CDBSidebarMenuItem>
+                                </a>
+                            </li>
+                            <li>
+                                <a href='https://esa.act.gov.au/cbr-be-emergency-ready/emergency-arrangements' target='none' rel='noopener noreferrer'>
+                                    <CDBSidebarMenuItem>Emergency</CDBSidebarMenuItem>
+                                </a>
+                            </li>
+                        </ul>
+
+                        <CDBSidebarMenuItem><hr className='sidebar-hr'/></CDBSidebarMenuItem>
 
                         <Link exact to='/quiz'>
                             <CDBSidebarMenuItem>Take a quiz</CDBSidebarMenuItem>
+                        </Link>
+
+                        <CDBSidebarMenuItem><hr className='sidebar-hr'/></CDBSidebarMenuItem>
+
+                        <Link exact to='/'>
+                            <CDBSidebarMenuItem>Home</CDBSidebarMenuItem>
                         </Link>
                     </CDBSidebarMenu>
                 </CDBSidebarContent>
--- ui/src/routes/Tutorial/Tutorial.js
+++ ui/src/routes/Tutorial/Tutorial.js
@@ -5,7 +5,7 @@ import React from "react";
 const Tutorial = () => {
     return (
         <div>
-            
+
         </div>
     );
 }
