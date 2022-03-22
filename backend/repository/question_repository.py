fd94143f7b179f76202ee23a111d36dc5f9d1d4e
wenghaoyu <bujue.why@gmail.com>
2022-03-22 11:14:13 +0800

    Add annotations

--- backend/repository/question_repository.py
+++ backend/repository/question_repository.py
@@ -1,9 +1,14 @@
 from domain import Question, QuestionType
 
-
+#
 def get_question_by_id(session, question_id):
+    """
+    search question by question id
+    :return: question object
+    """
     question = session.query(Question).filter(Question.id == question_id).first()
     if question and question.status == 1:
+        # We should use the object after this session closed
         session.expunge(question)
         return question
     else:
