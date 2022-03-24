9b8a190c4c2774a6852f12f5989cf0fee72a7908
pdsla112 <pdsla112@gmail.com>
2022-03-25 03:52:51 +1100

    Implement cards in a catalogue

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,3 +1,5 @@
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
--- ui/src/routes/QuizMainPage/QuizMainPage.js
+++ ui/src/routes/QuizMainPage/QuizMainPage.js
@@ -1,15 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './QuizMainPage.scss';
 
 import React from 'react';
-// import Card from 'react-bootstrap/Card';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 const QuizMainPage = () => {
     return (
         <div>
             <h1>Assessment Centre</h1>
             <h3>Choose your role</h3>
+            <Container>
+            <Row xs={1} lg={3} xxl={4}>
+                <Col>
+                    <Card>
+                        <Card.Img variant='top' src='https://www.rbgsyd.nsw.gov.au/getmedia/ce90c9e5-0e81-4904-94c8-5410a143bce7/placeholder_cross_1200x815.png'/>
+                        <Card.Body>
+                            <Card.Title>Role</Card.Title>
+                            <Card.Text>Role description...</Card.Text>
+                            <Button variant="danger">Proceed</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+            </Row>
+            </Container>
         </div>
     );
 };
