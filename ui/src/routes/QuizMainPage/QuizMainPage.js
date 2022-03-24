3d3a3a8b779003f707a9f707f836dca187e706ac
pdsla112 <pdsla112@gmail.com>
2022-03-24 13:22:15 +1100

    Add boileplate code for QuizMainPage

---
+++ ui/src/routes/QuizMainPage/QuizMainPage.js
@@ -0,0 +1,13 @@
+import './QuizMainPage.scss';
+
+import React from 'react';
+
+const QuizMainPage = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default QuizMainPage;
