6a123cb49a2d524dd610cc71ee896cd56d0289f8
Julia_YueGe <2406353205@qq.com>
2022-04-13 19:50:31 +0800

    fix a bug on different variable names.

--- backend/domain/entity/password_retrieval.py
+++ backend/domain/entity/password_retrieval.py
@@ -7,5 +7,5 @@ class PasswordRetrieval(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(256), name='email', unique=True)
     code = Column(String(256), name='code', nullable=False)
-    created_time = Column(DateTime, name='created_datetime', default=datetime.now(), nullable=False)
-    expired_time = Column(DateTime, name='expired_datetime', default=datetime.now(), nullable=False)
+    created_datetime = Column(DateTime, name='created_datetime', default=datetime.now(), nullable=False)
+    expired_datetime = Column(DateTime, name='expired_datetime', default=datetime.now(), nullable=False)
--- backend/services/authentication.py
+++ backend/services/authentication.py
@@ -116,8 +116,8 @@ class AuthenticationService():
             session.add(code_query)
         else:
             resend_user.code = generate_code
-            resend_user.created_time = datetime.now()
-            resend_user.expired_time = resend_user.created_datetime + timedelta(days=1)
+            resend_user.created_datetime = datetime.now()
+            resend_user.expired_datetime = resend_user.created_datetime + timedelta(days=1)
             session.commit()
         session.flush()
         return ForgotPassword.SUCCESS
@@ -136,7 +136,7 @@ class AuthenticationService():
         if query is None or query.code is None:
             return VerifyCode.FAIL
         now_time = datetime.now()
-        if now_time > query.expired_time:
+        if now_time > query.expired_datetime:
             return VerifyCode.CODE_OVERDUE
         if query.code == code:
             return VerifyCode.CODE_CORRECT
