4594713ac28357e4b0308172c651137884e594da
pdsla112 <pdsla112@gmail.com>
2022-04-29 06:18:58 +1000

    Implement static calendar

--- ui/src/routes/VolunteerCalendar/VolunteerCalendar.js
+++ ui/src/routes/VolunteerCalendar/VolunteerCalendar.js
@@ -1,11 +1,44 @@
 import './VolunteerCalendar.scss';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
 
+import moment from "moment";
 import React from 'react';
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+
+const DDCalendar = withDragAndDrop(Calendar);
+const localizer = momentLocalizer(moment);
+
+const events = [
+    {
+        title: 'Take exam',
+        start: new Date(2022, 3, 29, 1),
+        end: new Date(2022, 3, 29, 12)
+    },
+    {
+        title: 'Do audit 3',
+        start: new Date(2022, 3, 29),
+        end: new Date(2022, 3, 29)
+    },
+    {
+        title: 'Study',
+        start: new Date(2022, 3, 29, 13),
+        end: new Date(2022, 3, 30, 12)
+    }
+];
 
 const VolunteerCalendar = () => {
     return (
-        <div>
-            
+        <div className='calendar-body'>
+            <DDCalendar
+                localizer={localizer}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: '80vh' }}
+                events={events}
+                selectable={true}
+            />
         </div>
     );
 };
