d5c8130d5045079c4d8ae138a80c6254f5041f1a
James Hewitt <u6381610@anu.edu.au>
2022-05-03 19:22:05 +1000

    Form created

--- ui/src/routes/VolunteerCalendar/VolunteerCalendar.js
+++ ui/src/routes/VolunteerCalendar/VolunteerCalendar.js
@@ -43,17 +43,89 @@ const VolunteerCalendar = () => {
     }
 
     return (
-        <div className='calendar-body'>
-            <DDCalendar
-                localizer={localizer}
-                startAccessor="start"
-                endAccessor="end"
-                style={{ height: '80vh' }}
-                events={blocks}
-                selectable={true}
-                onEventDrop={handleEventChange}
-                onEventResize={handleEventChange}
-            />
+        <div className='X'>
+
+            {/* Calendar */}
+            <div className='calendar-body'>
+                <DDCalendar
+                    localizer={localizer}
+                    startAccessor="start"
+                    endAccessor="end"
+                    style={{ height: '80vh' }}
+                    events={blocks}
+                    selectable={true}
+                    onEventDrop={handleEventChange}
+                    onEventResize={handleEventChange}
+                />
+            </div>
+
+            {/* SideBar / Form */}
+            <aside>
+                <form
+                    className={'mt6 w-50 ml-auto mr-auto ba br2 b--black-10 pa3'}>
+
+                    <h2>Enter your unavailability</h2>
+
+                    <div className="form-group">
+                        <label htmlFor={'Name'}>Title:</label>
+                        <input
+                            className={'form-control'}
+                            type="text"
+                            name="name"
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor={'givenName'}>Reoccuring:</label>
+                        <input
+                            type="checkbox"
+                            name="reoccur"
+                        />
+                   </div>
+
+                    <div className="form-group">
+                      <label htmlFor="startTime">Choose a Start Time:</label>
+                      <select id="startTime" name="startTime">
+                        <option value="test">1am</option>
+                        <option value="test">2am</option>
+                        <option value="testtest">3am</option>
+                        <option value="test">4am</option>
+                        <option value="test">5am</option>
+                        <option value="testv">6am</option>
+                        <option value="test">7am</option>
+                        <option value="testtest">8am</option>
+                        <option value="test">9am</option>
+                        <option value="test">10am</option>
+                        <option value="test">11am</option>
+                        <option value="testv">12am</option>
+                      </select>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="endTime">Choose a End Time:</label>
+                      <select id="endTime" name="endTime">
+                        <option value="test">1am</option>
+                        <option value="test">2am</option>
+                        <option value="testtest">3am</option>
+                        <option value="test">4am</option>
+                        <option value="test">5am</option>
+                        <option value="testv">6am</option>
+                        <option value="test">7am</option>
+                        <option value="testtest">8am</option>
+                        <option value="test">9am</option>
+                        <option value="test">10am</option>
+                        <option value="test">11am</option>
+                        <option value="testv">12am</option>
+                      </select>
+                    </div>
+
+                    <input
+                      type="submit"
+                      value="Submit"
+                      className={'btn bg-light-red pv2 ph3 br2 b near-white dim'}
+                    />
+                </form>
+            </aside>
         </div>
     );
 };
