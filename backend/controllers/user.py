3eaffe433b531472ddfe85d256e2322b8bb18560
Haoyu Weng <u7170234@anu.edu.au>
2022-05-09 16:43:40 +0800

    Add: get user availabilities by id

--- backend/controllers/user.py
+++ backend/controllers/user.py
@@ -3,10 +3,9 @@ from flask_restful import fields, Resource, marshal_with, Api, reqparse
 
 from domain import session_scope
 from repository.user_repository import get_user_by_id
-from repository.user_role_repository import get_user_roles_by_id, get_user_roles
+from repository.user_role_repository import get_user_roles_by_id
 
-
-user_fields = {
+user_info_fields = {
     'name': fields.String,
     'email': fields.String,
     'mobile_number': fields.String,
@@ -14,8 +13,8 @@ user_fields = {
 }
 
 
-class GetUserRequest(Resource):
-    @marshal_with(user_fields)
+class GetUserInfoRequest(Resource):
+    @marshal_with(user_info_fields)
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('id', type=int, required=True)
@@ -31,6 +30,18 @@ class GetUserRequest(Resource):
             return user_dict
 
 
+class GetAvailabilitiesRequest(Resource):
+    @staticmethod
+    def get():
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True)
+        args = parser.parse_args()
+        with session_scope() as session:
+            user = get_user_by_id(session, args['id'])
+            return user.availabilities
+
+
 user_bp = Blueprint('user', __name__)
 api = Api(user_bp, "/user")
-api.add_resource(GetUserRequest, "/getUserById")
+api.add_resource(GetUserInfoRequest, "/getUserById")
+api.add_resource(GetAvailabilitiesRequest, "/getAvailabilityById")
